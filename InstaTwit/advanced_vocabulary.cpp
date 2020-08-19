54e6a1276b89923da096e28c9e518cf4f5d4fcf0
alimovlex <alimovlex@hotmail.com>
2020-08-19 19:02:58 +0300

    The second part of the GRE Magoosh advanced vocabulary has been processed!

--- InstaTwit/advanced_vocabulary.cpp
+++ InstaTwit/advanced_vocabulary.cpp
@@ -18,12 +18,35 @@ string CXXClass::advanced_first()
                                        "contentious", "lascivious", "supercilious", "chary", "pariah",
                                        "base", "saturnine", "ponderous", "perfunctory", "arrant",
                                        "truculent", "feckless", "overweening", "untoward", "catholic"};
-    vector<string> thoughts {"byzantine", "expansive", "insufferable", "factious", ""};
+    vector<string> thoughts {"byzantine", "expansive", "insufferable", "factious"};
     vector<string> objects {"alacrity", "malfeasance", "exegesis", "apogee", "nadir",
                             "vicissitude", "turpitude", "diatribe", "zeitgeist", "anathema"};
     vector<string> actions {"to defray", "to hector", "to kowtow", "to base", "to attenuate",
                             "to cow", "to imbibe", "to expunge", "to beg", "to conciliate",
                             "to equivocate", "to precipitate"};
+    vector<string> :: iterator itr;
+    vector<string> :: iterator jtr;
+    vector<string> :: iterator ktr;
+    vector<string> :: iterator ltr;
+    sentence=sentence_shuffle(personal_qualities, thoughts, objects, actions);
+    
+    return sentence;
+}
+
+string CXXClass::advanced_second()
+{
+    string sentence;
+    vector<string> personal_qualities {"sybarite", "parvenu", "impecunious", "arriviste", "blinkered",
+                                       "schadenfreude", "benighted", "martinet", "quisling", "bilious",
+                                       "jejune", "protean", "curmudgeon", "pollyannaish", "quixotic",
+                                       "mettlesome", "sartorial", "histrionic"};
+    vector<string> thoughts {"maudlin", "venial", "fell", "picayune", "Pyrrhic",
+                             "limpid", "factitious", "jaundiced", "phantasmagorical", "tendentious",
+                             "invective", "sententious", "peremptory", "arch", "mellifluous"};
+    vector<string> objects {"cupidity", "mulct", "sangfroid", "apotheosis", "juggernaut",
+                            "palimpsest", "malapropism", "imbroglio"};
+    vector<string> actions {"to hedge", "to propitiate", "to disabuse","to expurgate","to gerrymander",
+                            "to excoriate", "to remonstrate", "to vituperate", "to execrate"};
     vector<string> :: iterator itr;
     vector<string> :: iterator jtr;
     vector<string> :: iterator ktr;
