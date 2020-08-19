40ac36e04122f73675b72fa45797bb4a169a160e
alimovlex <alimovlex@hotmail.com>
2020-08-19 17:03:04 +0300

    The advanced first section of the GRE Magoosh vocabulary has been completed.

--- InstaTwit/advanced_vocabulary.cpp
+++ InstaTwit/advanced_vocabulary.cpp
@@ -13,10 +13,17 @@ using namespace std;
 string CXXClass::advanced_first()
 {
     string sentence;
-    vector<string> personal_qualities {};
-    vector<string> thoughts {};
-    vector<string> objects {};
-    vector<string> actions {};
+    vector<string> personal_qualities {"penurious", "indigent", "fractious", "sedulous", "jingoist",
+                                       "choleric", "prosaic", "artless", "churlish", "apostate",
+                                       "contentious", "lascivious", "supercilious", "chary", "pariah",
+                                       "base", "saturnine", "ponderous", "perfunctory", "arrant",
+                                       "truculent", "feckless", "overweening", "untoward", "catholic"};
+    vector<string> thoughts {"byzantine", "expansive", "insufferable", "factious", ""};
+    vector<string> objects {"alacrity", "malfeasance", "exegesis", "apogee", "nadir",
+                            "vicissitude", "turpitude", "diatribe", "zeitgeist", "anathema"};
+    vector<string> actions {"to defray", "to hector", "to kowtow", "to base", "to attenuate",
+                            "to cow", "to imbibe", "to expunge", "to beg", "to conciliate",
+                            "to equivocate", "to precipitate"};
     vector<string> :: iterator itr;
     vector<string> :: iterator jtr;
     vector<string> :: iterator ktr;
